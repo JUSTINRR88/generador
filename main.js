0c1cfe0a6ab2868010039e59abca18ac9c8db773
JUSTINRR88 <justinbercian1@gmail.com>
2024-06-10 10:36:38 -0600

    version 1

---
+++ main.js
@@ -0,0 +1,10 @@
+function generarContrasena() {
+    const longitud = 18; // Longitud de la contraseña
+    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+    let contrasena = '';
+    for (let i = 0; i < longitud; i++) {
+        const indice = Math.floor(Math.random() * caracteres.length);
+        contrasena += caracteres.charAt(indice);
+    }
+    document.getElementById('password').value = contrasena;
+}
